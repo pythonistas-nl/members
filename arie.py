0e77f6ce0a65333c68b5419b963c2bf3e4e09945
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-05-29 20:09:43 +0200

    add personal description

---
+++ arie.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+
+__author__ = 'Arie Bovenberg'
+__github__ = 'ariebovenberg'
+
+EXPERIENCE = timedelta(days=5*365)
+
+KNOWLEDGE_LEVEL = {
+    'git': 4,
+    'vim': 4,
+    'django': 4,
+    'scikit-learn': 3,
+    'numpy': 2,
+    'scipy': 2,
+}
+
+INTERESTED_IN = [
+    'functional programming',
+    'neural networks',
+    'build your own X',
+]
